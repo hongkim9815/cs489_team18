0492fb6f9586b9554d71fefdbbff60bcb294b5b1
hongkim9815 <hongkim9815@gmail.com>
2019-12-01 17:39:15 +0900

    [v1.2] Change interface; Add search function;

--- script.js
+++ script.js
@@ -9,27 +9,48 @@ function submit_data_func(){
       contentType: false,
       processData: false,
       success: function(data){
-        console.log(data);
         datalist = data.split("---");
-        console.log(datalist);
-        content = '';
-        var i = 0;
-        for(i = 0; i < datalist.length - 4; i++)
+        document.getElementById('result').innerText = data;
+        if(datalist.length < 5)
         {
-          content += datalist[i];
+          if (datalist[0] == "NO VOCA")
+          {
+            document.getElementById('word').innerText = $('#name').val();
+            document.getElementById('result').innerHTML =
+            'There is no "'+datalist[1]+'"<br><br>'
+            + 'Please, add a meaning of the word...'
+            + '<br>'
+            + '<textarea class="text" name="mean" style="width:100%; height:100%"></textarea>'
+            + '<br>'
+            + '<center><input type="submit" id="submit" value="Send Request"></center>';
+            $('#submit').click(function(event){
+              alert("Submitted!");
+              submit_data_func();
+            });
+          }
+          else
+            document.getElementById('result').innerText = "ERROR : " + data;
         }
-        document.getElementById('result').innerText = content;
-        document.getElementById('good').innerText = datalist[i];
-        document.getElementById('bad').innerText = datalist[i+1];
-        document.getElementById('good_recent').innerText = datalist[i+2];
-        document.getElementById('bad_recent').innerText = datalist[i+3];
+        else
+        {
+          content = '';
+          var i = 0;
+          for(i = 0; i < datalist.length - 4; i++)
+          {
+            content += datalist[i];
+          }
+          document.getElementById('word').innerText = $('#name').val();
+          document.getElementById('result').innerText = content;
+          document.getElementById('good').innerText = datalist[i];
+          document.getElementById('bad').innerText = datalist[i+1];
+          document.getElementById('good_recent').innerText = datalist[i+2];
+          document.getElementById('bad_recent').innerText = datalist[i+3];
+        }
       },
     });
   }
 
 document.addEventListener('DOMContentLoaded', function(){
-  var submit_button = document.getElementById('submit_button');
-  submit_button.addEventListener('click', submit_data_func);
   chrome.tabs.getSelected(null, function(tab){
     chrome.tabs.executeScript(tab.id, {code: "window.getSelection().toString();"}, function (blocked){
       if(blocked[0].length > 0)
@@ -39,16 +60,14 @@ document.addEventListener('DOMContentLoaded', function(){
       }
     });
   });
-  document.body.addEventListener('dblclick', function(){
-    console.log(window.getSelection().toString());
-  });
-  document.body.addEventListener('click', function(){
-    console.log(window.getSelection().toString());
-  });
 });
 
-$('input[type="text"]').keydown(function() {
-  if (event.keyCode == 13){
-    submit_data()
-  }
+$(document).ready(function() {
+  $('#name').keydown(function(event){
+    submit_data_func();
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
 });
